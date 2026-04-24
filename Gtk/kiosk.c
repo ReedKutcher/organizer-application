7488dec65ef869ab2ffc4e422d94ad8978f31375
ReedKutcher <reed_kutcher@students.nksd.net>
2026-04-24 00:08:45 -0400

    Added clock and reformatted css for customizability

--- Gtk/kiosk.c
+++ Gtk/kiosk.c
@@ -1,6 +1,17 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-gboolean on_key_pressed(GtkEventControllerKey *controller, guint keyval, guint keycode, GdkModifierType state, gpointer userData) {
+struct DateTimeFmt {
+    GtkWidget *dateLabel;
+    GtkWidget *timeLabel;
+
+    char *dateFmt;
+    char *timeFmt;
+};
+
+gboolean onKeyPressed(GtkEventControllerKey *controller, guint keyval, guint keycode, GdkModifierType state, gpointer userData) {
     if (keyval == GDK_KEY_F4) {
         gtk_window_close(userData);
         return 1;
@@ -12,23 +23,49 @@ static void printHello(GtkWidget *widget, gpointer data) {
     gtk_revealer_set_reveal_child(data, gtk_revealer_get_child_revealed(data) ? FALSE : TRUE);
 }
 
+gboolean everySecond(gpointer data) {
+    GDateTime *dateTime;
+    struct DateTimeFmt *formatData;
+    
+    formatData = (struct DateTimeFmt *)data;
+    dateTime = g_date_time_new_now_local();
+
+    gtk_label_set_text(GTK_LABEL(formatData->dateLabel), g_date_time_format(dateTime, formatData->dateFmt));
+    gtk_label_set_text(GTK_LABEL(formatData->timeLabel), g_date_time_format(dateTime, formatData->timeFmt));
+
+    return TRUE;
+}
+
+void freeEverySecond(gpointer data) {
+    g_free(data);
+}
+
 static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window;
-    GtkWidget *menuButton;
-    GtkWidget *bannerSeparator;
-    GtkWidget *menuSeparator;
-    GtkWidget *bannerFrame;
-    GtkWidget *menuFrame;
-    GtkWidget *menuRevealer;
-    GtkWidget *contentFrame;
+    GtkCssProvider *provider = NULL;
 
-    GtkCssProvider *provider;
+    GtkWidget *window = NULL;
 
-    GdkCursor *cursor;
-    GtkEventController *controller;
+    GdkCursor *cursor = NULL;
+    GtkEventController *controller = NULL;
+    
+    GtkWidget *contentFrame = NULL;
+
+    GtkWidget *bannerSeparator = NULL;
+    GtkWidget *bannerFrame = NULL;
+    GtkWidget *bannerBox = NULL;
+    GtkWidget *menuButton = NULL;
+    GtkWidget *dateLabel = NULL;
+    GtkWidget *timeLabel = NULL;
+
+    GtkWidget *menuSeparator = NULL;
+    GtkWidget *menuRevealer = NULL;
+    GtkWidget *menuFrame = NULL;
+
+    struct DateTimeFmt *defaultDateTime = (struct DateTimeFmt *)malloc(sizeof(struct DateTimeFmt));
+
 
     provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "styles.css");
+    gtk_css_provider_load_from_path(provider, "gtk.css");
     gtk_style_context_add_provider_for_display(gdk_display_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     window = gtk_application_window_new(app);
@@ -36,11 +73,12 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_default_size(GTK_WINDOW(window), 1080, 1920);
 
     controller = gtk_event_controller_key_new();
-    g_signal_connect(controller, "key-pressed", G_CALLBACK(on_key_pressed), GTK_WINDOW(window));
+    g_signal_connect(controller, "key-pressed", G_CALLBACK(onKeyPressed), GTK_WINDOW(window));
     gtk_widget_add_controller(window, controller);
 
     cursor = gdk_cursor_new_from_name("arrow", NULL);
     gtk_widget_set_cursor(window, cursor);
+
 
     bannerSeparator = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_window_set_child(GTK_WINDOW(window), bannerSeparator);
@@ -49,6 +87,10 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_box_append(GTK_BOX(bannerSeparator), bannerFrame);
     gtk_widget_add_css_class(bannerFrame, "banner");
     gtk_widget_set_hexpand(bannerFrame, TRUE);
+
+    bannerBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_frame_set_child(GTK_FRAME(bannerFrame), bannerBox);
+
 
     menuSeparator = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_box_append(GTK_BOX(bannerSeparator), menuSeparator);
@@ -68,13 +110,31 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_box_append(GTK_BOX(menuSeparator), contentFrame);
     gtk_widget_add_css_class(contentFrame, "content");
 
+
     menuButton = gtk_button_new_from_icon_name("open-menu-symbolic");
     gtk_widget_set_halign(menuButton, GTK_ALIGN_START);
     gtk_widget_set_valign(menuButton, GTK_ALIGN_START);
     gtk_widget_add_css_class(menuButton, "menu-button");
     g_signal_connect(menuButton, "clicked", G_CALLBACK(printHello), GTK_REVEALER(menuRevealer));
-    gtk_frame_set_child(GTK_FRAME(bannerFrame), menuButton);
+    gtk_box_append(GTK_BOX(bannerBox), menuButton);
 
+    dateLabel = gtk_label_new("Date Loading...");
+    gtk_widget_set_hexpand(dateLabel, TRUE);
+    gtk_widget_add_css_class(dateLabel, "date");
+
+    timeLabel = gtk_label_new("Time Loading...");
+    gtk_widget_add_css_class(timeLabel, "time");
+
+    defaultDateTime->dateLabel = dateLabel;
+    defaultDateTime->timeLabel = timeLabel;
+    defaultDateTime->dateFmt = "%A, %B %d, %Y";
+    defaultDateTime->timeFmt = "%I:%M %p";
+    g_timeout_add_full(G_PRIORITY_DEFAULT, 1000, G_SOURCE_FUNC(everySecond), defaultDateTime, freeEverySecond);
+
+    gtk_box_append(GTK_BOX(bannerBox), dateLabel);
+    gtk_box_append(GTK_BOX(bannerBox), timeLabel);
+
+    
     gtk_window_present(GTK_WINDOW(window));
 }
 
